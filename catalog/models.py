c8579cdeebad16525ea0437173edf20fa76639ab
Brendan Henry <Brendan_Henry_business@outlook.com>
2017-08-23 22:35:07 +0800

    admin site init

--- catalog/models.py
+++ catalog/models.py
@@ -20,7 +20,7 @@ class Book(models.Model):
     summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
     isbn = models.CharField('ISBN',max_length=13,help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN Number</a>')
     genre = models.ManyToManyField(Genre, help_text="Select a genre of this book")
-
+    
     def __str__(self):
         return self.title
 
@@ -29,6 +29,10 @@ class Book(models.Model):
         Return the url to access a particular book instance.
         """
         return reverse('book-detail', args=[str(self.id)])
+
+    def display_genre(self):
+        return ', '.join([ genre.name for genre in self.genre.all()[:3] ])
+    display_genre.short_description = 'Genre'
 
 import uuid 
 
